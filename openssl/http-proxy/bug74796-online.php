48fef4ddd482ebb647b8c77c98cce235fa804cf1
Jakub Zelenka <bukka@php.net>
2025-04-09 15:58:56 +0200

    Update ssl http proxy test

--- openssl/http-proxy/bug74796-online.php
+++ openssl/http-proxy/bug74796-online.php
@@ -2,11 +2,11 @@
 
 stream_context_set_default([
   "http" => [
-    "proxy" => "tcp://127.0.0.1:10000"
+    "proxy" => "tcp://127.0.0.1:10001"
     ],
 ]);
-//var_dump(stream_context_get_options(stream_context_get_default()));
-file_get_contents("https://www.example.com/"); // fine
-// var_dump(stream_context_get_options(stream_context_get_default())); // ssl:peer_name is set
-// file_get_contents("https://www.yahoo.com/"); // Peer certificate CN=`*.www.yahoo.com' did not match expected CN=`www.google.com' in ...
-// var_dump(stream_context_get_options(stream_context_get_default()));
+var_dump(stream_context_get_options(stream_context_get_default()));
+var_dump(file_get_contents("https://www.google.com/")); // fine
+var_dump(stream_context_get_options(stream_context_get_default())); // ssl:peer_name is set
+var_dump(file_get_contents("https://www.yahoo.com/")); // Peer certificate CN=`*.www.yahoo.com' did not match expected CN=`www.google.com' in ...
+var_dump(stream_context_get_options(stream_context_get_default()));
